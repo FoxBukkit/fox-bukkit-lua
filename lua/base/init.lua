c5a126343666eeb6bb385ea24b5ed4f39404a06d
Mark Dietzer <mriq91@gmail.com>
2015-06-01 13:01:51 +0200

    Rename project

--- lua/base/init.lua
+++ lua/base/init.lua
@@ -1,19 +1,19 @@
 --[[
 
-    This file is part of FoxBukkit.
+    This file is part of FoxBukkitLua.
 
-    FoxBukkit is free software: you can redistribute it and/or modify
+    FoxBukkitLua is free software: you can redistribute it and/or modify
     it under the terms of the GNU Lesser General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
 
-    FoxBukkit is distributed in the hope that it will be useful,
+    FoxBukkitLua is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU Lesser General Public License for more details.
 
     You should have received a copy of the GNU Lesser General Public License
-    along with FoxBukkit.  If not, see <http://www.gnu.org/licenses/>.
+    along with FoxBukkitLua.  If not, see <http://www.gnu.org/licenses/>.
 
 ]]
 package.path = __ROOTDIR__ .. "/classes/?.lua;" .. __ROOTDIR__ .. "/?.lua"
