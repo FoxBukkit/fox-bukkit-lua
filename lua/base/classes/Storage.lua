a0675e3dc59107039d3dae6716e9e1e4f1af948c
Mark Dietzer <mriq91@gmail.com>
2015-06-01 13:00:11 +0200

    Woo license headers

--- lua/base/classes/Storage.lua
+++ lua/base/classes/Storage.lua
@@ -1,3 +1,21 @@
+--[[
+
+    This file is part of FoxBukkit.
+
+    FoxBukkit is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    FoxBukkit is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with FoxBukkit.  If not, see <http://www.gnu.org/licenses/>.
+
+]]
 local _entity_mt = {
 	__index = function(tbl, idx)
 		local storageTbl = rawget(tbl, 'storage')
