4ee49fbc5140dedf0215c8a109e92ae3f6a35679
Mark Dietzer <mriq91@gmail.com>
2015-06-04 12:46:30 +0200

    Better event handling

--- src/main/resources/lua/classes/Event.lua
+++ src/main/resources/lua/classes/Event.lua
@@ -22,20 +22,51 @@ local eventPriority = luajava.bindClass("org.bukkit.event.EventPriority")
 
 local type = type
 
-return {
+local class
+
+local _event_mt = {
+    __index = {
+        register = function(self)
+            return class:register(self)
+        end,
+        unregister = function(self)
+            return class:unregister(self)
+        end
+    },
+    __newindex = function()
+        error("Readonly")
+    end,
+    __metatable = false
+}
+
+class = {
 	register = function(self, event, priority, callback, ignoreCancelled)
-		if type(event) == "string" then
-			event = luajava.bindClass(event)
+		if not event.__info then
+			event.__info = {}
+			if type(event.class) == "string" then
+				event.class = luajava.bindClass(event.class)
+			end
+			event.priority = event.priority or eventPriority.NORMAL
+			if type(event.priority) == "string" then
+				event.priority = eventPriority[event.Priority:upper()]
+			end
+			event.ignoreCancelled = event.ignoreCancelled or false
+			event = setmetatable(event, _event_mt)
 		end
-		if type(priority) == "string" then
-			priority = eventPriority[priority:upper()]
-		end
-		return eventManager:register(event, priority, ignoreCancelled or false, callback)
+		event.__info.listener = eventManager:register(event.class, event.priority, event.ignoreCancelled, function(evt)
+			return event:run(evt)
+		end)
+		return event
 	end,
 
-	unregister = function(self, listener)
-		return eventManager:unregister(listener)
+	unregister = function(self, event)
+		if event.__info.listener then
+			eventManager:unregister(event.__info.listener)
+			event.__info.listener = nil
+		end
 	end,
 
 	Priority = eventPriority
 }
+
+return class
