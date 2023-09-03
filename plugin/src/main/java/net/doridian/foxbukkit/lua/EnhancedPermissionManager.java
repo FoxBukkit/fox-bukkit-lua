061e54a2d2af3d1270d3535d01cd65396dcd534a
Doridian <git@doridian.net>
2023-09-03 00:47:26 -0700

    first adjust

--- plugin/src/main/java/net/doridian/foxbukkit/lua/EnhancedPermissionManager.java
+++ plugin/src/main/java/net/doridian/foxbukkit/lua/EnhancedPermissionManager.java
@@ -1,20 +1,21 @@
-/**
- * This file is part of FoxBukkitLua-plugin.
+/*
+ * foxbukkit-lua-plugin - ${project.description}
+ * Copyright © ${year} Doridian (git@doridian.net)
  *
- * FoxBukkitLua-plugin is free software: you can redistribute it and/or modify
+ * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * FoxBukkitLua-plugin is distributed in the hope that it will be useful,
+ * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with FoxBukkitLua-plugin.  If not, see <http://www.gnu.org/licenses/>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package com.foxelbox.foxbukkit.lua;
+package net.doridian.foxbukkit.lua;
 
 import com.foxelbox.foxbukkit.permissions.FoxBukkitPermissionHandler;
 import com.foxelbox.foxbukkit.permissions.FoxBukkitPermissions;
